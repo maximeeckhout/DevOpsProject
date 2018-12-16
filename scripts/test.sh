89eee27d09d14c58decede525e1860d0cec2aa4b
EwoutMaertens <maertens.ewout@hotmail.com>
2018-12-16 11:34:13 +0000

    Delete test.sh

--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-until [ -s "/var/lib/jenkins/workspace/DevOps Backend/test.txt" ]
-	do
-		echo "hello"
-		sleep 5
-		echo "test"  > "/var/lib/jenkins/workspace/DevOps Backend/test.txt" 
-	done
